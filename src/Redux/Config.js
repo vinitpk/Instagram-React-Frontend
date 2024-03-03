abcf2f30e20c2c7d74bbd6c01be49204656c10e2
vinitpk <vinitpk00@gmail.com>
2024-03-03 21:05:11 +0530

    update url

--- src/Redux/Config.js
+++ src/Redux/Config.js
@@ -1,5 +1,6 @@
-export const BASE_API_URL = "http://localhost:5050";
-export const COMMENT_API_URL = "http://localhost:5050/api/comments";
-export const POST_API_URL = "http://localhost:5050/api/posts";
-export const STORY_API_URL = "http://localhost:5050/api/stories";
-export const USER_API_URL = "http://localhost:5050/api/users";
+// export const BASE_API_URL = "http://localhost:5050";
+export const BASE_API_URL = "https://instagram-springboot-backend.onrender.com";
+export const COMMENT_API_URL = `${BASE_API_URL}/api/comments`;
+export const POST_API_URL = `${BASE_API_URL}/api/posts`;
+export const STORY_API_URL = `${BASE_API_URL}/api/stories`;
+export const USER_API_URL = `${BASE_API_URL}/api/users`;
