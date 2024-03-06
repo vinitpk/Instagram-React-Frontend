b9d98b7f7d583ae21b3aa9eb58f357441a5d6315
vinitpk <vinitpk00@gmail.com>
2024-03-06 12:02:09 +0530

    added aws config

--- src/Redux/Config.js
+++ src/Redux/Config.js
@@ -1,5 +1,6 @@
-// export const BASE_API_URL = "http://localhost:5050";
-export const BASE_API_URL = "https://instagram-springboot-backend.onrender.com";
+// export const BASE_API_URL = "http://localhost:5050"; 
+export const BASE_API_URL = "http://ec2-13-48-192-10.eu-north-1.compute.amazonaws.com:8080"; 
+// export const BASE_API_URL = "https://instagram-springboot-backend.onrender.com";
 export const COMMENT_API_URL = `${BASE_API_URL}/api/comments`;
 export const POST_API_URL = `${BASE_API_URL}/api/posts`;
 export const STORY_API_URL = `${BASE_API_URL}/api/stories`;
