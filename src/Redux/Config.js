723f600622a3f378a34cee8b70609c59fc637f3f
vinitpk <vinitpk00@gmail.com>
2024-03-06 12:14:06 +0530

    added aws config

--- src/Redux/Config.js
+++ src/Redux/Config.js
@@ -1,6 +1,6 @@
 // export const BASE_API_URL = "http://localhost:5050";
 export const BASE_API_URL =
-    "https://ec2-13-48-192-10.eu-north-1.compute.amazonaws.com:8080";
+    "http://ec2-13-48-192-10.eu-north-1.compute.amazonaws.com:8080";
 export const COMMENT_API_URL = `${BASE_API_URL}/api/comments`;
 export const POST_API_URL = `${BASE_API_URL}/api/posts`;
 export const STORY_API_URL = `${BASE_API_URL}/api/stories`;
